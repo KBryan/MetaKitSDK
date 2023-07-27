f53a61d18bd54d4d04826bc241d19a7fd43964b3
kwame.eth <kwame.bryan@gmail.com>
2023-07-27 16:43:43 -0400

    Update Script.cs
    Removed disabled API key

--- Assets/Scenes/Script.cs
+++ Assets/Scenes/Script.cs
@@ -10,7 +10,7 @@ public class Script : MonoBehaviour
     private JsonRpcProvider _rpc;
     public async void  Start()
     {
-        _rpc = new JsonRpcProvider("https://goerli.infura.io/v3/b574497921744232a0c128d304243fd1");
+        _rpc = new JsonRpcProvider("https://goerli.infura.io/v3/ADD_API_KEY");
         var block = await _rpc.GetBlock();
         Debug.Log("Block Data: " + JsonConvert.SerializeObject(block,Formatting.Indented));
         Wallet wallet = new Wallet(Wordlist.English, WordCount.Twelve);
